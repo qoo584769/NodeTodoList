156b9163e5b10ae273b9580c6008b5a12f5dc97a
ming wu <a85062@pchome.com.tw>
2022-03-01 22:47:09 +0800

    修復BUG

--- index.js
+++ index.js
@@ -15,110 +15,113 @@ const RequestListen = (req, res) => {
     body += chunk;
   });
   // -------------------------------------------------
-  // 路由名稱錯誤
-  if (req.url !== '/todos') {
-    HttpMethod(res, 404, 'false', todos, '路由錯誤,請加上/todos路由');
-    return;
-  }
-  switch (req.method) {
-    // -------------------------------------------------
-    // GET 取得全部代辦事項
-    case 'GET':
-      HttpMethod(res, 200, 'success', todos, '成功取得清單');
-      break;
-    // -------------------------------------------------
-    // POST 新增代辦事項
-    case 'POST':
-      req.on('end', () => {
-        try {
-          const data = JSON.parse(body).data;
-          const NewData = [];
+  // 路由名稱正確
+  if (req.url.startsWith('/todos/') || req.url == '/todos') {
+    switch (req.method) {
+      // -------------------------------------------------
+      // GET 取得全部代辦事項
+      case 'GET':
+        HttpMethod(res, 200, 'success', todos, '成功取得清單');
+        break;
+      // -------------------------------------------------
+      // POST 新增代辦事項
+      case 'POST':
+        req.on('end', () => {
+          try {
+            const data = JSON.parse(body).data;
+            const NewData = [];
 
-          data.forEach((val, ind) => {
-            if (val.title !== undefined) {
-              const todo = {
-                title: val.title,
-                id: uuidv4(),
-              };
-              NewData.push(todo);
+            data.forEach((val, ind) => {
+              if (val.title !== undefined) {
+                const todo = {
+                  title: val.title,
+                  id: uuidv4(),
+                };
+                NewData.push(todo);
+                return;
+              }
+              throw new Error(`新增失敗,第 ${ind + 1} 筆Key值不是title`);
+            });
+
+            todos = todos.concat(NewData);
+            HttpMethod(
+              res,
+              200,
+              'success',
+              todos,
+              `新增 ${NewData.length} 筆資料成功`
+            );
+          } catch (err) {
+            if (err instanceof SyntaxError) {
+              HttpMethod(res, 404, 'false', todos, '非JSON格式');
               return;
             }
-            throw new Error(`新增失敗,第 ${ind + 1} 筆Key值不是title`);
-          });
-
-          todos = todos.concat(NewData);
-          HttpMethod(
-            res,
-            200,
-            'success',
-            todos,
-            `新增 ${NewData.length} 筆資料成功`
-          );
-        } catch (err) {
-          if (err instanceof SyntaxError) {
-            HttpMethod(res, 404, 'false', todos, '非JSON格式');
-            return;
+            HttpMethod(res, 404, 'false', todos, err.message);
+          } finally {
           }
-          HttpMethod(res, 404, 'false', todos, err.message);
-        } finally {
-        }
-      });
-      break;
-    // -------------------------------------------------
-    // PATCH 更改某比代辦事項
-    case 'PATCH':
-      req.on('end', () => {
-        try {
-          const title = JSON.parse(body).title;
-          const id = req.url.split('/').pop();
+        });
+        break;
+      // -------------------------------------------------
+      // PATCH 更改某比代辦事項
+      case 'PATCH':
+        req.on('end', () => {
+          try {
+            const title = JSON.parse(body).title;
+            const id = req.url.split('/').pop();
+            const index = todos.findIndex((ind) => ind.id == id);
+
+            if (title == undefined) {
+              HttpMethod(res, 404, 'false', todos, '鍵值 title 未定義');
+              return;
+            } else if (index == -1) {
+              HttpMethod(res, 404, 'false', todos, '找無此筆資料');
+              return;
+            }
+
+            todos[index].title = title;
+            HttpMethod(res, 200, 'success', todos, `已更新 id : ${id} 資料`);
+          } catch {
+            HttpMethod(res, 404, 'false', todos, '非JSON格式');
+          }
+        });
+        break;
+      // -------------------------------------------------
+      // DELETE 刪除單筆/全部代辦事項
+      case 'DELETE':
+        const id = req.url.split('/').pop();
+
+        if (id !== 'todos') {
           const index = todos.findIndex((ind) => ind.id == id);
 
-          if (title == undefined) {
-            HttpMethod(res, 404, 'false', todos, '鍵值 title 未定義');
-            return;
-          } else if (index == -1) {
+          if (index == -1) {
             HttpMethod(res, 404, 'false', todos, '找無此筆資料');
             return;
           }
 
-          todos[index].title = title;
-          HttpMethod(res, 200, 'success', todos, `已更新 id : ${id} 資料`);
-        } catch {
-          HttpMethod(res, 404, 'false', todos, '非JSON格式');
+          todos.splice(index, 1);
+
+          HttpMethod(res, 200, 'success', todos, `已刪除 id : ${id} 資料`);
+        } else {
+          todos.length = 0;
+
+          HttpMethod(res, 200, 'success', todos, '已刪除全部資料');
         }
-      });
-      break;
-    // -------------------------------------------------
-    // DELETE 刪除單筆/全部代辦事項
-    case 'DELETE':
-      const id = req.url.split('/').pop();
+        break;
+      // -------------------------------------------------
+      // OPTIONS 確認跨網域是否有問題
+      case 'OPTIONS':
+        res.writeHeader(200, headers);
+        res.end();
+        break;
 
-      if (id !== 'todos') {
-        const index = todos.findIndex((ind) => ind.id == id);
-
-        if (index == -1) {
-          HttpMethod(res, 404, 'false', todos, '找無此筆資料');
-          return;
-        }
-
-        todos.splice(index, 1);
-
-        HttpMethod(res, 200, 'success', todos, `已刪除 id : ${id} 資料`);
-      } else {
-        todos.length = 0;
-
-        HttpMethod(res, 200, 'success', todos, '已刪除全部資料');
-      }
-      break;
-    // -------------------------------------------------
-    // OPTIONS 確認跨網域是否有問題
-    case 'OPTIONS':
-      res.writeHeader(200, headers);
-      res.end();
-      break;
-
-    default:
-      break;
+      default:
+        break;
+    }
+  } 
+  // -------------------------------------------------
+  // 路由名稱錯誤
+  else {
+    HttpMethod(res, 404, 'false', todos, '路由錯誤,請加上/todos路由');
   }
   // -------------------------------------------------
   // GET 取得全部代辦事項
