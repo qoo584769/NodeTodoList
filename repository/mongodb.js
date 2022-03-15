ef88512ed44e38709e691de5cbb170e167beafd6
ming wu <a85062@pchome.com.tw>
2022-03-15 20:23:06 +0800

    【更新】 修復dotenv套件衝突

--- repository/mongodb.js
+++ repository/mongodb.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const { MongoClient } = require('mongodb');
 const uri = process.env.MONGO_DB_CONNECTING_STRING;
 const client = new MongoClient(uri);
